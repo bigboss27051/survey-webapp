7cf09b14a1fc5cadd7c84795005396788c1e28ec
bigboss27051 <tanawut.bigboss@gmail.com>
2020-09-22 01:34:09 +0700

    fix conflict

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,7 @@
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
         <v-card-text>
-<<<<<<< HEAD
           <p>{{token}}
-           
-=======
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >documentation</a>.
->>>>>>> 79b39da84abb1bfaf24d3990c50a000b60c7a70c
           </p>
           <p>
             If you have questions, please join the official
