7ded117f455cc2983c290c7b9ea6179a450fe230
Jedsada Saengow <jedsada.ngow@gmail.com>
2021-03-27 10:12:43 +0700

    edit nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,16 +3,45 @@
     <v-btn icon>
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
-    <v-navigation-drawer v-model="drawer" :mini-variant="false" :clipped="clipped" fixed app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="false"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text" />
-          </v-list-item-content>
-        </v-list-item>
+        <v-list-group
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          id="list-group-vending"
+          no-action
+          value="true"
+        >
+          <template v-slot:activator>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text" />
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="(child, cIndex) in item.children"
+            link
+            :key="cIndex"
+            :to="child.to"
+          >
+            <v-list-item-action>
+              <v-icon>mdi-label</v-icon>
+            </v-list-item-action>
+            <v-list-item-title v-text="child.text"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar color="primary" :clipped-left="clipped" fixed app>
@@ -39,7 +68,7 @@
 <script>
 export default {
   props: {
-    source: String
+    source: String,
   },
   data: () => ({
     dialog: false,
@@ -47,15 +76,36 @@ export default {
     drawer: true,
     fixed: true,
     items: [
-      { icon: "mdi-check-circle", text: "Survey", to: "/survey/createSurvey" },
-      { icon: "mdi-logout", text: "Logout", to: "/" }
+      // {
+      //   icon: "mdi-check-circle",
+      //   text: "Survey",
+      //   to: "/survey/createSurvey",
+      // },
+      {
+        icon: "mdi-check-circle",
+        text: "Survey",
+        to: "/survey/createSurvey",
+        children: [
+          {
+            icon: "mdi-check-circle",
+            text: "Create survey",
+            to: "/survey/createSurvey",
+          },
+          {
+            icon: "mdi-check-circle",
+            text: "View survey",
+            to: "/survey/createSurvey",
+          },
+        ],
+      },
+      { icon: "mdi-logout", text: "Logout", to: "/" },
     ],
-    title: "DQ Survey"
+    title: "DQ Survey",
   }),
   methods: {
     goToLogin() {
       this.$router.push("/authentication/login");
-    }
-  }
+    },
+  },
 };
 </script>
